b1f106354c04ecf9c2e969b7efc619ad0cd746b6
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-12 00:56:05 -0300

    Organize imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+import "./assets/scss/main.scss";
 import App from "./routes/router";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
