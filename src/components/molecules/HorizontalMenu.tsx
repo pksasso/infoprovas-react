6d9b0dbfc14d983bbb9a0c81b3ce11217f063149
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-14 22:52:24 -0300

    Add key to prevent warning

--- src/components/molecules/HorizontalMenu.tsx
+++ src/components/molecules/HorizontalMenu.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { MenuItem } from "../atoms/MenuItem";
 
 export const HorizontalMenu = ({ items }: { items: MenuItem[] }) => {
-  const horizontalMenuItems = items.map((item) => (
-    <MenuItem path={item.path} name={item.name}></MenuItem>
+  const horizontalMenuItems = items.map((item, index) => (
+    <MenuItem key={index} path={item.path} name={item.name}></MenuItem>
   ));
 
   return <nav className="container-menu">{horizontalMenuItems}</nav>;
