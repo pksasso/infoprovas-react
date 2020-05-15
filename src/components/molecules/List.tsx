909b1d691b4567f22fb2a6e0e48c40c06c7b48b8
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-15 01:12:40 -0300

    Add first example of panels

--- src/components/molecules/List.tsx
+++ src/components/molecules/List.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { ListItem } from "../atoms/ListItem";
 
-export const List = ({ List }: { List: any[] }): JSX.Element => {
-  const items = List.map((item, index) => (
+export const List = ({ data }: { data: any[] }): JSX.Element => {
+  const items = data.map((item, index) => (
     <ListItem key={index} item={item}></ListItem>
   ));
 
--- src/components/templates/Template.tsx
+++ src/components/templates/Template.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { List } from "../molecules/List";
 import { Panel } from "../molecules/Panel";
 
 export const TemplatePage = ({ title }: { title: string }) => {
   const items = [];
 
-  for (let index = 0; index < 12; index++) {
-    items.push(<Panel key={index} title={`${title}_${index + 1}`} />);
+  for (let index = 0; index < 8; index++) {
+    items.push(
+      <Panel
+        key={index}
+        title={`${title}_${index + 1}`}
+        content={<List data={["Ola", "Mundo"]} />}
+      />
+    );
   }
 
   return <div className="page-content">{items}</div>;
