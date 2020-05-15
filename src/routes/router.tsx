4ea22222960ee43e107dd6f5a127dc7f71bc40cc
pksasso <patricksassorj@gmail.com>
2020-05-15 18:34:49 -0300

    Add logo svg

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/logo.svg";
 import { Header } from "../components/organisms/Header";
 import { HORIZONTAL_NAVIGATION_MENU } from "../constants/menu";
 import { Pages } from "./Switch";
