df1b7dd066031b50f823a6ed398ed760b143c444
pksasso <patricksassorj@gmail.com>
2020-05-15 20:51:15 -0300

    Add send button style

--- src/components/atoms/SendButton.tsx
+++ src/components/atoms/SendButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import "../../assets/scss/atoms/SendButton.scss";
 
 export const SendButton = (handleSend: any) => {
-  return <button onClick={() => handleSend}>Enviar</button>;
+  return <button className="send-button" onClick={() => handleSend}>Enviar</button>;
 };
