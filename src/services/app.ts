70121128dc8bec0b4b458147a83db757266dcc09
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-17 23:04:03 -0300

    Add service methods

--- src/services/app.ts
+++ src/services/app.ts
@@ -1,8 +1,78 @@
-import API from "./API";
+import infoProvasAPI from "./API";
 
 const InfoProvasService = {
-  listSubjects({ id }: { id: number }) {
-    return API().get(`/api/courses/${id}/subjects`);
+  fetchSubjects({ courseId }: { courseId: number }) {
+    return infoProvasAPI.get(`/api/courses/${courseId}/subjects`);
+  },
+  fetchCourses() {
+    return infoProvasAPI.get("api/courses");
+  },
+  fetchExamTypes() {
+    return infoProvasAPI.get("api/exam_types");
+  },
+  fetchExamsBySubject({
+    courseId,
+    subjectId,
+  }: {
+    courseId: number;
+    subjectId: number;
+  }) {
+    return infoProvasAPI.get(
+      `api/courses/${courseId}/subjects/${subjectId}/exams`
+    );
+  },
+  fetchExamFile({
+    courseId,
+    subjectId,
+    examId,
+  }: {
+    courseId: number;
+    subjectId: number;
+    examId: number;
+  }) {
+    return infoProvasAPI.get(
+      `api/courses/${courseId}/subjects/${subjectId}/exams/${examId}/file`
+    );
+  },
+  fetchExamsByProfessor({
+    courseId,
+    professorId,
+  }: {
+    courseId: number;
+    professorId: number;
+  }) {
+    return infoProvasAPI.get(
+      `api/courses/${courseId}/professors/${professorId}/exams`
+    );
+  },
+  getExamByProfessor({
+    courseId,
+    professorId,
+    examId,
+  }: {
+    courseId: number;
+    professorId: number;
+    examId: number;
+  }) {
+    return infoProvasAPI.get(
+      `api/courses/${courseId}/professors/${professorId}/exams/${examId}`
+    );
+  },
+  getExamFileByProfessor({
+    courseId,
+    professorId,
+    examId,
+  }: {
+    courseId: number;
+    professorId: number;
+    examId: number;
+  }) {
+    return infoProvasAPI.get(
+      `api/courses/${courseId}/professors/${professorId}/exams/${examId}/file`
+    );
+  },
+  fetchProfessorsByCourse({ courseId }: { courseId: number }) {
+    return infoProvasAPI.get(`api/courses/${courseId}/professors`);
   },
 };
 
