1582c8943d0acf23a9b6c4a8672db3d64ebe7275
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-18 00:01:42 -0300

    Add raw loading

--- src/components/pages/Disciplines.tsx
+++ src/components/pages/Disciplines.tsx
@@ -4,16 +4,24 @@ import { DisciplinesTemplate } from "../templates/Disciplines";
 
 export const DisciplinesPage = ({ courseId = 1 }: { courseId?: number }) => {
   const [items, setItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const getSubjects = async () => {
       const { fetchSubjects } = InfoProvasService;
+
+      setIsLoaded(true);
       const response = await fetchSubjects({ courseId });
       setItems(response.data);
+      setIsLoaded(false);
     };
 
     getSubjects();
   }, [courseId]);
+
+  if (isLoaded) {
+    return <div>Loading...</div>;
+  }
 
   return <DisciplinesTemplate disciplines={items} />;
 };
