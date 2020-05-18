eb3617ea33b32881f225382de9f3b5754699d067
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-17 21:56:49 -0300

    Fix Discipline model

--- src/models/Discipline.ts
+++ src/models/Discipline.ts
@@ -1,7 +1,8 @@
-export interface Disciplines {
+import { Resource } from "./Resource";
+
+export interface Disciplines extends Resource {
   code: string;
-  courseId: number;
-  id: number;
+  course_id: number;
   name: string;
   semester: number;
 }
