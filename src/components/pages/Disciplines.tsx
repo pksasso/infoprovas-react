21c3e547410c8c595ac1021f666d448cb52d2e0a
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-18 00:10:33 -0300

    Add raw error

--- src/components/pages/Disciplines.tsx
+++ src/components/pages/Disciplines.tsx
@@ -4,20 +4,30 @@ import { DisciplinesTemplate } from "../templates/Disciplines";
 
 export const DisciplinesPage = ({ courseId = 1 }: { courseId?: number }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const getSubjects = async () => {
       const { fetchSubjects } = InfoProvasService;
 
-      setIsLoaded(true);
-      const response = await fetchSubjects({ courseId });
-      setItems(response.data);
-      setIsLoaded(false);
+      try {
+        setIsLoaded(true);
+        const response = await fetchSubjects({ courseId });
+        setItems(response.data);
+        setIsLoaded(false);
+      } catch (err) {
+        console.log(err);
+        setError(err);
+      }
     };
 
     getSubjects();
   }, [courseId]);
+
+  if (error) {
+    return <div>Error</div>;
+  }
 
   if (isLoaded) {
     return <div>Loading...</div>;
