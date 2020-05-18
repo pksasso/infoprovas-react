8f05f3a982de001544ed78eaeba79ddac8a5d524
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-05-17 21:55:29 -0300

    Fix base api requester

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-axios.defaults.headers.Accept = "application/json";
-axios.defaults.headers["Content-Type"] = "application/json";
-axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
-
 const API = () => {
-  return axios.create({ baseURL: process.env.REACT_APP_HOST_API });
+  return axios.create({
+    baseURL: process.env.REACT_APP_HOST_API,
+    withCredentials: false,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
 };
 
 export default API;
