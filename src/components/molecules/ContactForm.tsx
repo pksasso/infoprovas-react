bf85c8d095a0a5269fd494ced9184f2a4092758c
Patrick Sasso <patrickasssorj@gmail.com>
2020-05-27 22:12:31 -0300

    Contact form changes

--- src/components/molecules/ContactForm.tsx
+++ src/components/molecules/ContactForm.tsx
@@ -1,28 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
 import { ActionButton } from "../atoms/ActionButton";
+import { Dropdown } from "../atoms/Dropdown";
+import { TextArea } from "../atoms/TextArea";
 import { TextBox } from "../atoms/TextBox";
 
-export const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-
+export const ContactForm = ({
+  send,
+  name,
+  setName,
+  email,
+  setEmail,
+  subject,
+  setSubject,
+  message,
+  setMessage,
+}: {
+  send: any;
+  name: string;
+  setName: any;
+  email: string;
+  setEmail: any;
+  subject: string;
+  setSubject: any;
+  message: string;
+  setMessage: any;
+}) => {
   const sendEmail = (e: React.FormEvent<EventTarget>): void => {
     e.preventDefault();
-    console.log(name);
-    console.log(email);
-    console.log(subject);
-    console.log(message);
+    send();
   };
 
   return (
-    <form onSubmit={(e) => sendEmail(e)}>
-      <TextBox name="nome" value={name} setText={setName} />
-      <TextBox name="Email" value={email} setText={setEmail} />
-      <TextBox name="Assunto" value={subject} setText={setSubject} />
-      <TextBox name="Mensagem" value={message} setText={setMessage} />
-      <ActionButton title="Enviar" onClick={sendEmail} />
-    </form>
+    <div className=" column is-offset-1 is-10">
+      <form onSubmit={(e) => sendEmail(e)}>
+        <TextBox name="Nome" value={name} setText={setName} maxLength={255} />
+        <TextBox
+          name="Email"
+          value={email}
+          setText={setEmail}
+          maxLength={255}
+        />
+        <Dropdown setSelected={setSubject} />
+        <TextArea
+          label="Mensagem"
+          placeholder="Descreva aqui seu problema"
+          maxLength={5000}
+          value={message}
+          setMessage={setMessage}
+        />
+        <div className="columns column is-12 is-centered">
+          <ActionButton title="Enviar" onClick={sendEmail} />
+        </div>
+      </form>
+    </div>
   );
 };
