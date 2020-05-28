2c1aa3817231be7f78a5d89fcd207e8429133653
Patrick Sasso <patrickasssorj@gmail.com>
2020-05-27 22:11:22 -0300

    Button fix

--- src/components/atoms/ActionButton.tsx
+++ src/components/atoms/ActionButton.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 export const ActionButton = ({
-  className,
   onClick,
   title,
 }: {
-  className?: string;
   title: string;
   onClick?: (fn: any) => void;
 }) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button className="button is-primary" onClick={onClick}>
       {title}
     </button>
   );
