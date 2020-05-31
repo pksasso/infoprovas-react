968cbd7a60c672517bed0b6046f635b067119b43
pksasso <patricksassorj@gmail.com>
2020-05-31 16:18:48 -0300

    Panel changes

--- src/components/molecules/Panel.tsx
+++ src/components/molecules/Panel.tsx
@@ -4,14 +4,18 @@ export const Panel = ({
   title,
   content,
 }: {
-  title: string;
+  title?: string;
   content: JSX.Element;
 }) => {
+  const header = (
+    <header className="panel-heading has-background-primary has-text-white-ter">
+      <p>{title}</p>
+    </header>
+  );
+
   return (
     <div className="panel">
-      <header className="panel-heading has-background-primary has-text-white-ter">
-        <p>{title}</p>
-      </header>
+      {title ? header : <div></div>}
       <section className="panel-block has-background-white">{content}</section>
     </div>
   );
