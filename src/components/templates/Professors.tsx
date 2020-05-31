da3fa22bb5a19eb54bb74d43da08dae5848a1f09
pksasso <patricksassorj@gmail.com>
2020-05-31 16:20:23 -0300

    Add professor template

---
+++ src/components/templates/Professors.tsx
@@ -0,0 +1,37 @@
+import _ from "lodash";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Professor } from "../../models/Professor";
+import { List } from "../molecules/List";
+import { Panel } from "../molecules/Panel";
+
+export const ProfessorsTemplate = ({
+  professors,
+}: {
+  course?: number;
+  professors: Professor[];
+}) => {
+  const professorsPanel: JSX.Element[] = [];
+
+  const alphabeticalOrder = _.orderBy(professors, ["name"], ["asc"]);
+
+  const createLink = (professorId: number) => {
+    return `professor/${professorId}`;
+  };
+
+  const professorLinks = alphabeticalOrder.map((professor) => {
+    return (
+      <Link key={professor.id} to={createLink(professor.id)}>
+        {professor.name}
+      </Link>
+    );
+  });
+
+  professorsPanel.push(
+    <div className="column is-8" key="professor">
+      <Panel content={<List data={professorLinks} />} />
+    </div>
+  );
+
+  return <div className="columns is-centered">{professorsPanel}</div>;
+};
