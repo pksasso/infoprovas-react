7069e66a16544a925fc229848771861f3c24d076
pksasso <patricksassorj@gmail.com>
2020-06-01 19:29:44 -0300

    Add professor exams page

---
+++ src/components/pages/ProfessorExams.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { Professor } from "../../models/Professor";
+import InfoProvasService from "../../services/app";
+import { ProfessorExamsTemplate } from "../templates/ProfessorExams";
+
+export const ProfessorExamsPage = ({
+  courseId = 1,
+  professorId,
+}: {
+  courseId?: number;
+  professorId: number;
+}) => {
+  const [items, setItems] = useState([]);
+  const [professor, setProfessor] = useState<Professor | undefined>();
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const getProfessorExams = async () => {
+      const { fetchExamsByProfessor } = InfoProvasService;
+
+      try {
+        setIsLoaded(true);
+        const response = await fetchExamsByProfessor({ courseId, professorId });
+        setItems(response.data);
+        setIsLoaded(false);
+      } catch (err) {
+        console.log(err);
+        setError(err);
+      }
+    };
+
+    const getProfessorData = async () => {
+      const { getProfessorDetailsByCourse } = InfoProvasService;
+      try {
+        const response = await getProfessorDetailsByCourse({
+          courseId,
+          professorId,
+        });
+        setProfessor(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getProfessorExams();
+    getProfessorData();
+  }, [courseId, professorId]);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  if (isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return <ProfessorExamsTemplate exams={items} professor={professor} />;
+};
