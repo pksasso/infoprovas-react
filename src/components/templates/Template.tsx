8602eff1f8194347efc0ee0999613edd46f12f12
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-06-06 01:58:03 -0300

    Add under construction message

--- src/components/templates/Template.tsx
+++ src/components/templates/Template.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import { List } from "../molecules/List";
-import { Panel } from "../molecules/Panel";
 
 export const TemplatePage = ({ title }: { title: string }) => {
-  const items = [];
-
-  for (let index = 0; index < 8; index++) {
-    items.push(
-      <div key={index} className="column is-4">
-        <Panel
-          title={`${title}_${index + 1}`}
-          content={<List data={["Ola", "Mundo"]} />}
-        />
-      </div>
-    );
-  }
-
   return (
     <div className="container is-fullhd">
-      <div className="columns is-multiline is-centered">{items}</div>
+      <div className="columns is-multiline is-centered">
+        <div className="column">
+          <div className="content">
+            <h1 className="is-title has-text-centered has-text-grey">
+              Em construção
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
