d224829134acca6dc21f5d8ea77fcf71d12ecae5
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-06-07 16:12:06 -0300

    Adjust template message

--- src/components/templates/Template.tsx
+++ src/components/templates/Template.tsx
@@ -6,9 +6,10 @@ export const TemplatePage = ({ title }: { title: string }) => {
       <div className="columns is-multiline is-centered">
         <div className="column">
           <div className="content">
-            <h1 className="is-title has-text-centered has-text-grey">
+            <h1 className="title has-text-centered has-text-grey">{title}</h1>
+            <h2 className="subtitle has-text-centered has-text-grey">
               Em construção
-            </h1>
+            </h2>
           </div>
         </div>
       </div>
