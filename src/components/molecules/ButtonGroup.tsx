21502e5b117329fcc8623e6a971f577629a407e2
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-06-07 12:00:03 -0300

    Fix buttons

--- src/components/molecules/ButtonGroup.tsx
+++ src/components/molecules/ButtonGroup.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const ButtonGroup = () => {
   const [active, setActive] = useState(true);
   const [active2, setActive2] = useState(false);
+
+  const { pathname: currentPath } = useLocation();
 
   return (
     <div className="buttons is-centered">
@@ -13,7 +15,9 @@ export const ButtonGroup = () => {
           setActive2(false);
         }}
         className={
-          active ? "button is-rounded is-primary" : "button is-rounded is-info"
+          active || currentPath === "/semestre"
+            ? "button is-rounded is-primary"
+            : "button is-rounded is-info"
         }
         to="/semestre"
       >
@@ -25,7 +29,9 @@ export const ButtonGroup = () => {
           setActive2(true);
         }}
         className={
-          active2 ? "button is-rounded is-primary" : "button is-rounded is-info"
+          active2 || currentPath === "/professores"
+            ? "button is-rounded is-primary"
+            : "button is-rounded is-info"
         }
         to="/professores"
       >
