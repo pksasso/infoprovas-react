7487e907e23df998b18747c52854e7664809d6a3
pksasso <patricksassorj@gmail.com>
2020-06-08 20:53:57 -0300

    Add send exam page

---
+++ src/components/pages/SendExam.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import InfoProvasService from "../../services/app";
+import { SendExamTemplate } from "../templates/SendExam";
+
+export const SendExamPage = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [courseId, setCourseId] = useState(1);
+  const [examTypes, setExamTypes] = useState([]);
+  const [professors, setProfessors] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const getProfessors = async () => {
+      const { fetchProfessorsByCourse } = InfoProvasService;
+      try {
+        setIsLoaded(true);
+        const response = await fetchProfessorsByCourse({ courseId });
+        setProfessors(response.data);
+        setIsLoaded(false);
+      } catch (err) {
+        console.log(err);
+        setError(err);
+      }
+    };
+
+    const getSubjects = async () => {
+      const { fetchSubjects } = InfoProvasService;
+      try {
+        setIsLoaded(true);
+        const response = await fetchSubjects({ courseId });
+        setSubjects(response.data);
+        setIsLoaded(false);
+      } catch (err) {
+        console.log(err);
+        setError(err);
+      }
+    };
+
+    const getExamTypes = async () => {
+      const { fetchExamTypes } = InfoProvasService;
+      try {
+        setIsLoaded(true);
+        const response = await fetchExamTypes();
+        setExamTypes(response.data);
+        setIsLoaded(false);
+      } catch (err) {
+        console.log(err);
+        setError(err);
+      }
+    };
+
+    getSubjects();
+    getProfessors();
+    getExamTypes();
+  }, [courseId]);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  if (isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <SendExamTemplate
+      subjects={subjects}
+      professors={professors}
+      examTypes={examTypes}
+    />
+  );
+};
