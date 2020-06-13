0577836f3045e1bed4399c9c78cc04d4b2e19e06
Patrick <patricksassorj@gmail.com>
2020-06-12 21:11:24 -0300

    Add Help page

---
+++ src/components/pages/Help.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { questions } from '../../constants/questions';
+import QuestionAnswer from '../molecules/QuestionAnswer';
+
+export const HelpPage = () => {
+
+  return (
+    <div className="container is-fullhd">
+      <div className="columns">
+        <div className="column is-offset-2 is-8 panel has-background-white">
+          <h1 className="title columns column is-12 is-centered">Ajuda</h1>
+          {questions.map(question => {
+            return (
+              <QuestionAnswer
+                question={question.question}
+                answer={question.answer}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
