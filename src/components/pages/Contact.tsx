bf9c973a835c19e1bd01feae0ef9bbba21056bb5
Patrick <patricksassorj@gmail.com>
2020-06-28 19:28:17 -0300

    Add contact form loader

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import InfoProvasService from "../../services/app";
+import { Loader } from '../atoms/Loader';
 import { ContactForm } from "../molecules/ContactForm";
 
 export const Contact = () => {
@@ -7,28 +8,51 @@ export const Contact = () => {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
 
-  const sendEmail = () => {
+  const sendEmail = async () => {
     const { postContact } = InfoProvasService;
-    postContact({ name, email, subject, message });
+
+    setIsLoading(true);
+
+    await postContact({ name, email, subject, message }).then((res) => {
+      setIsLoading(false);
+      if (res.status === 200 && res.data.success === "Message submitted") {
+        alert('Mensagem enviada !!!');
+        clearFields();
+      }
+    })
   };
+
+  const clearFields = () => {
+    setEmail("");
+    setName("");
+    setMessage("");
+    setSubject("");
+  }
 
   return (
     <div className="container is-fullhd">
       <div className="columns">
         <div className="column is-offset-2 is-8 panel has-background-white">
           <h1 className="title columns column is-12 is-centered">Contato</h1>
-          <ContactForm
-            send={sendEmail}
-            name={name}
-            setName={setName}
-            email={email}
-            setEmail={setEmail}
-            subject={subject}
-            setSubject={setSubject}
-            message={message}
-            setMessage={setMessage}
-          />
+          {
+            isLoading
+              ?
+              <Loader />
+              :
+              <ContactForm
+                send={sendEmail}
+                name={name}
+                setName={setName}
+                email={email}
+                setEmail={setEmail}
+                subject={subject}
+                setSubject={setSubject}
+                message={message}
+                setMessage={setMessage}
+              />
+          }
         </div>
       </div>
     </div>
