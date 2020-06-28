676f8ea2a0cebc13f7853bb29c153043d290dc4c
Patrick <patricksassorj@gmail.com>
2020-06-28 19:21:20 -0300

    Add Loader component

---
+++ src/components/atoms/Loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Loader = (
+) => {
+  return (
+    <div className="cssload-container">
+      <div className="cssload-speeding-wheel"></div>
+    </div>
+  );
+};
