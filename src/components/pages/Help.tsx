af0df32e226ef5a48d5530c0a5545040bcb7020a
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2020-06-29 23:19:36 -0300

    Add index to helper

--- src/components/pages/Help.tsx
+++ src/components/pages/Help.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { questions } from '../../constants/questions';
-import QuestionAnswer from '../molecules/QuestionAnswer';
+import React from "react";
+import { questions } from "../../constants/questions";
+import QuestionAnswer from "../molecules/QuestionAnswer";
 
 export const HelpPage = () => {
-
   return (
     <div className="container is-fullhd">
       <div className="columns">
         <div className="column is-offset-2 is-8 panel has-background-white">
           <h1 className="title columns column is-12 is-centered">Ajuda</h1>
-          {questions.map(question => {
+          {questions.map((question, index) => {
             return (
               <QuestionAnswer
+                key={index}
                 question={question.question}
                 answer={question.answer}
               />
-            )
+            );
           })}
         </div>
       </div>
     </div>
   );
-}
+};
