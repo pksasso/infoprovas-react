91796b63aa97147411c6495a0d08b5d7f1b06ab6
pksasso <patricksassorj@gmail.com>
2020-07-04 16:21:20 -0300

    Add ActionButton type

--- src/components/atoms/ActionButton.tsx
+++ src/components/atoms/ActionButton.tsx
@@ -3,12 +3,14 @@ import React from "react";
 export const ActionButton = ({
   onClick,
   title,
+  type = "button",
 }: {
   title: string;
   onClick?: (fn: any) => void;
+  type?: "submit" | "button";
 }) => {
   return (
-    <button className="button is-primary" onClick={onClick}>
+    <button className="button is-primary" onClick={onClick} type={type}>
       {title}
     </button>
   );
